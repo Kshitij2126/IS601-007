d6581810999f3e84b480e45710994601df1a30ac
KSHITIJ AJI <ka598@njit@edu>
2022-12-15 15:10:05 -0500

    minor updates

--- IS601ShopProject/auth/auth.py
+++ IS601ShopProject/auth/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, flash, redirect, url_for,current_app, session
+from flask_principal import Identity, AnonymousIdentity, \
+    identity_changed
+from flask import Blueprint, render_template, flash, redirect, url_for, current_app, session
 from auth.forms import LoginForm, ProfileForm, RegisterForm
 from accounts.accounts import get_or_create_account
 from sql.db import DB
@@ -10,10 +12,9 @@ from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
 
-from flask_principal import Identity, AnonymousIdentity, \
-     identity_changed
 
-auth = Blueprint('auth', __name__, url_prefix='/',template_folder='templates')
+auth = Blueprint('auth', __name__, url_prefix='/', template_folder='templates')
+
 
 def check_duplicate(e):
 
@@ -25,7 +26,8 @@ def check_duplicate(e):
         flash("Unknown error occurred, please try again", "danger")
         print(e)
 
-@auth.route("/register", methods=["GET","POST"])
+
+@auth.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
     # wtform validators are both client-side and server-side
@@ -36,28 +38,32 @@ def register():
         try:
             hash = bcrypt.generate_password_hash(password)
             # save the hash, not the plaintext password
-            result = DB.insertOne("INSERT INTO IS601_Users (email, username, password) VALUES (%s, %s, %s)", email, username, hash)
+            result = DB.insertOne(
+                "INSERT INTO IS601_Users (email, username, password) VALUES (%s, %s, %s)", email, username, hash)
             if result.status:
-                flash("Successfully registered","success")
+                flash("Successfully registered", "success")
         except Exception as e:
             check_duplicate(e)
     return render_template("register.html", form=form)
+
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         is_valid = True
-        email = form.email.data # email or username
+        email = form.email.data  # email or username
         password = form.password.data
         if is_valid:
             try:
-                result = DB.selectOne("SELECT id, email, username, password FROM IS601_Users where email= %(email)s or username=%(email)s", {"email":email})
+                result = DB.selectOne(
+                    "SELECT id, email, username, password FROM IS601_Users where email= %(email)s or username=%(email)s", {"email": email})
                 if result.status and result.row:
                     hash = result.row["password"]
                     if bcrypt.check_password_hash(hash, password):
                         from roles.models import Role
-                        del result.row["password"] # don't carry password/hash beyond here
+                        # don't carry password/hash beyond here
+                        del result.row["password"]
                         user = User(**result.row)
                         # get roles
                         result = DB.selectAll("""
@@ -72,12 +78,13 @@ def login():
                         account = get_or_create_account(user.id)
                         user.account = Account(**account)
 
-                        success = login_user(user) # login the user via flask_login
-                        
+                        # login the user via flask_login
+                        success = login_user(user)
+
                         if success:
                             # Tell Flask-Principal the identity changed
                             identity_changed.send(current_app._get_current_object(),
-                                    identity=Identity(user.id))
+                                                  identity=Identity(user.id))
                             # store user object in session as json
                             session["user"] = user.toJson()
                             flash("Log in successful", "success")
@@ -95,16 +102,17 @@ def login():
                 flash(str(e), "danger")
     return render_template("login.html", form=form)
 
+
 @auth.route("/landing-page", methods=["GET"])
 @login_required
 def landing_page():
-    
     return render_template("landing_page.html")
+
 
 @auth.route("/logout", methods=["GET"])
 def logout():
     logout_user()
-     # Remove session keys set by Flask-Principal
+    # Remove session keys set by Flask-Principal
     for key in ('identity.name', 'identity.auth_type'):
         session.pop(key, None)
 
@@ -113,6 +121,7 @@ def logout():
                           identity=AnonymousIdentity())
     flash("Successfully logged out", "success")
     return redirect(url_for("auth.login"))
+
 
 @auth.route("/profile", methods=["GET", "POST"])
 @login_required
@@ -129,33 +138,38 @@ def profile():
         # handle password change only if all 3 are provided
         if current_password and password and confirm:
             try:
-                result = DB.selectOne("SELECT password FROM IS601_Users where id = %s", user_id)
+                result = DB.selectOne(
+                    "SELECT password FROM IS601_Users where id = %s", user_id)
                 if result.status and result.row:
                     # verify current password
                     if bcrypt.check_password_hash(result.row["password"], current_password):
                         # update new password
                         hash = bcrypt.generate_password_hash(password)
                         try:
-                            result = DB.update("UPDATE IS601_Users SET password = %s WHERE id = %s", hash, user_id)
+                            result = DB.update(
+                                "UPDATE IS601_Users SET password = %s WHERE id = %s", hash, user_id)
                             if result.status:
                                 flash("Updated password", "success")
                         except Exception as ue:
                             flash(ue, "danger")
                     else:
-                        flash("Invalid password","danger")
+                        flash("Invalid password", "danger")
             except Exception as se:
                 flash(se, "danger")
-        
+
         if is_valid:
-            try: # update email, username (this will trigger if nothing changed but it's fine)
-                result = DB.update("UPDATE IS601_Users SET email = %s, username = %s WHERE id = %s", email, username, user_id)
+            # update email, username (this will trigger if nothing changed but it's fine)
+            try:
+                result = DB.update(
+                    "UPDATE IS601_Users SET email = %s, username = %s WHERE id = %s", email, username, user_id)
                 if result.status:
                     flash("Saved profile", "success")
             except Exception as e:
                 check_duplicate(e)
     try:
         # get latest info if anything changed
-        result = DB.selectOne("SELECT id, email, username FROM IS601_Users where id = %s", user_id)
+        result = DB.selectOne(
+            "SELECT id, email, username FROM IS601_Users where id = %s", user_id)
         if result.status and result.row:
             user = User(**result.row)
             # switch how user is loaded so we don't lose error validations
