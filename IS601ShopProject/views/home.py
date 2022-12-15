705d21da094cf313ecc66612f4302bf6655dc5dc
KSHITIJ AJI <ka598@njit@edu>
2022-12-15 10:55:45 -0500

    Updated home page html

--- IS601ShopProject/views/home.py
+++ IS601ShopProject/views/home.py
@@ -2,8 +2,6 @@ from flask import Blueprint, render_template
 home = Blueprint('home', __name__, url_prefix='/')
 
 # TODO DO NOT EDIT
-
-
 @home.route('/')
 def index():
     return render_template("home_page.html")
