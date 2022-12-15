6ad6a01828ee1f1cbd84deca5ed4a9ad02e4fba1
KSHITIJ AJI <ka598@njit@edu>
2022-12-15 14:17:51 -0500

    Changed Shop.py

--- IS601ShopProject/shop/shop.py
+++ IS601ShopProject/shop/shop.py
@@ -5,44 +5,47 @@ from shop.forms import ItemForm
 from sql.db import DB
 from roles.permissions import admin_permission
 from flask_login import login_required, current_user
-shop = Blueprint('shop', __name__, url_prefix='/',template_folder='templates')
+shop = Blueprint('shop', __name__, url_prefix='/', template_folder='templates')
 
-@shop.route("/admin/item", methods=["GET","POST"])
+
+@shop.route("/admin/item", methods=["GET", "POST"])
 @admin_permission.require(http_exception=403)
 def item():
     form = ItemForm()
     id = request.args.get("id", form.id.data or None)
     type = "Edit" if id else "Create"
     if form.validate_on_submit():
-        if form.id.data: # it's an update
+        if form.id.data:  # it's an update
             try:
                 result = DB.update("UPDATE IS601_S_Items set name = %s, description = %s, stock = %s, cost = %s, image=%s WHERE id = %s",
-                form.name.data, form.description.data, form.stock.data, form.cost.data, form.image.data, form.id.data)
+                                   form.name.data, form.description.data, form.stock.data, form.cost.data, form.image.data, form.id.data)
                 if result.status:
                     flash(f"Updated {form.name.data}", "success")
             except Exception as e:
                 print("Error updating item", e)
                 flash(f"Error updating item {form.name.data}", "danger")
-        else: # it's a create
+        else:  # it's a create
             try:
                 result = DB.update("""INSERT INTO IS601_S_Items (name, description, stock, cost, image) 
                 VALUES (%s,%s,%s,%s,%s)""",
-                form.name.data, form.description.data, form.stock.data, form.cost.data, form.image.data)
+                                   form.name.data, form.description.data, form.stock.data, form.cost.data, form.image.data)
                 if result.status:
                     flash(f"Created {form.name.data}", "success")
-                    form = ItemForm() # clear form
+                    form = ItemForm()  # clear form
             except Exception as e:
                 print("Error creating item", e)
                 flash(f"Error creating item {form.name.data}", "danger")
     if id:
         try:
-            result = DB.selectOne("SELECT id, name, description, stock, cost, image FROM IS601_S_Items WHERE id = %s", id)
+            result = DB.selectOne(
+                "SELECT id, name, description, stock, cost, image FROM IS601_S_Items WHERE id = %s", id)
             if result.status and result.row:
-                    form.process(MultiDict(result.row))
+                form.process(MultiDict(result.row))
         except Exception as e:
             print("Error fetching item", e)
             flash("Item not found", "danger")
     return render_template("item.html", form=form, type=type)
+
 
 @shop.route("/admin/items/delete", methods=["GET"])
 @admin_permission.require(http_exception=403)
@@ -54,16 +57,18 @@ def delete():
             if result.status:
                 flash("Deleted item", "success")
         except Exception as e:
-            print("Error deleting item",e)
+            print("Error deleting item", e)
             flash("Error deleting item", "danger")
     return redirect(url_for("shop.items"))
 
-@shop.route("/admin/items", methods=["GET","POST"])
+
+@shop.route("/admin/items", methods=["GET", "POST"])
 @admin_permission.require(http_exception=403)
 def items():
     rows = []
     try:
-        result = DB.selectAll("SELECT id, name, description, stock, cost, image FROM IS601_S_Items LIMIT 25",)
+        result = DB.selectAll(
+            "SELECT id, name, description, stock, cost, image FROM IS601_S_Items LIMIT 25",)
         if result.status and result.rows:
             rows = result.rows
     except Exception as e:
@@ -71,12 +76,14 @@ def items():
         flash("There was a problem loading items", "danger")
     return render_template("items.html", rows=rows)
 
-@shop.route("/shop", methods=["GET","POST"])
+
+@shop.route("/shop", methods=["GET", "POST"])
 @login_required
 def shop_list():
     rows = []
     try:
-        result = DB.selectAll("SELECT id, name, description, stock, cost, image FROM IS601_S_Items WHERE stock > 0 LIMIT 25",)
+        result = DB.selectAll(
+            "SELECT id, name, description, stock, cost, image FROM IS601_S_Items WHERE stock > 0 LIMIT 25",)
         if result.status and result.rows:
             rows = result.rows
     except Exception as e:
@@ -84,7 +91,8 @@ def shop_list():
         flash("There was a problem loading items", "danger")
     return render_template("shop.html", rows=rows)
 
-@shop.route("/cart", methods=["GET","POST"])
+
+@shop.route("/cart", methods=["GET", "POST"])
 @login_required
 def cart():
     item_id = request.form.get("item_id")
@@ -95,47 +103,51 @@ def cart():
     if id and user_id:
         if quantity > 0:
             try:
-                result = DB.selectOne("SELECT cost,name from IS601_S_Items WHERE id = %s", id)
+                result = DB.selectOne(
+                    "SELECT cost,name from IS601_S_Items WHERE id = %s", id)
                 print("result", result)
                 if result.status and result.row:
                     cost = result.row["cost"]
                     name = result.row["name"]
-                    if item_id: # update from cart
+                    if item_id:  # update from cart
                         result = DB.insertOne("""
                         UPDATE IS601_S_Cart SET
                         quantity = %(quantity)s,
                         cost = %(cost)s
                         WHERE item_id = %(id)s and user_id = %(user_id)s
-                        """,{
-                            "id":id,
+                        """, {
+                            "id": id,
                             "quantity": quantity,
-                            "cost":cost,
-                            "user_id":user_id
+                            "cost": cost,
+                            "user_id": user_id
                         })
                         if result.status:
-                            flash(f"Updated quantity for {name} to {quantity}", "success")
-                    else: #add from shop
+                            flash(
+                                f"Updated quantity for {name} to {quantity}", "success")
+                    else:  # add from shop
                         result = DB.insertOne("""
                         INSERT INTO IS601_S_Cart (item_id, quantity, cost, user_id)
                         VALUES(%(id)s, %(quantity)s, %(cost)s, %(user_id)s)
                         ON DUPLICATE KEY UPDATE
                         quantity = quantity + %(quantity)s,
                         cost = %(cost)s
-                        """,{
-                            "id":id,
+                        """, {
+                            "id": id,
                             "quantity": quantity,
-                            "cost":cost,
-                            "user_id":user_id
+                            "cost": cost,
+                            "user_id": user_id
                         })
                         if result.status:
-                            flash(f"Added {quantity} of {name} to cart", "success")
+                            flash(
+                                f"Added {quantity} of {name} to cart", "success")
             except Exception as e:
-                print("Error updating cart" ,e)
+                print("Error updating cart", e)
                 flash("Error updating cart", "danger")
         else:
             # assuming delete
             try:
-                result = DB.delete("DELETE FROM IS601_S_Cart where item_id = %s and user_id = %s", id, user_id)
+                result = DB.delete(
+                    "DELETE FROM IS601_S_Cart where item_id = %s and user_id = %s", id, user_id)
                 if result.status:
                     flash("Deleted item from cart", "success")
             except Exception as e:
@@ -154,7 +166,8 @@ def cart():
         flash("Error fetching cart", "danger")
     return render_template("cart.html", rows=rows)
 
-@shop.route("/purchase", methods=["GET","POST"])
+
+@shop.route("/purchase", methods=["GET", "POST"])
 @login_required
 def purchase():
     cart = []
@@ -162,10 +175,10 @@ def purchase():
     quantity = 0
     order = {}
     try:
-        DB.getDB().autocommit = False # make a transaction
+        DB.getDB().autocommit = False  # make a transaction
 
         # get cart to verify
-        
+
         result = DB.selectAll("""SELECT c.id, item_id, name, c.quantity, i.stock, c.cost as cart_cost, i.cost as item_cost, (c.quantity * c.cost) as subtotal 
         FROM IS601_S_Cart c JOIN IS601_S_Items i on c.item_id = i.id
         WHERE c.user_id = %s
@@ -176,10 +189,12 @@ def purchase():
         has_error = False
         for item in cart:
             if item["quantity"] > item["stock"]:
-                flash(f"Item {item['name']} doesn't have enough stock left", "warning")
+                flash(
+                    f"Item {item['name']} doesn't have enough stock left", "warning")
                 has_error = True
             if item["cart_cost"] != item["item_cost"]:
-                flash(f"Item {item['name']}'s price has changed, please refresh cart", "warning")
+                flash(
+                    f"Item {item['name']}'s price has changed, please refresh cart", "warning")
                 has_error = True
             total += int(item["subtotal"] or 0)
             quantity += int(item["quantity"])
@@ -208,7 +223,7 @@ def purchase():
             # Note: Not really an insert 1, it'll copy data from Table B into Table A
             result = DB.insertOne("""INSERT INTO IS601_S_OrderItems (quantity, cost, order_id, item_id, user_id)
             SELECT quantity, cost, %s, item_id, user_id FROM IS601_S_Cart c WHERE c.user_id = %s""",
-            order_id, current_user.get_id())
+                                  order_id, current_user.get_id())
             if not result.status:
                 flash("Error recording order history", "danger")
                 has_error = True
@@ -219,7 +234,7 @@ def purchase():
             UPDATE IS601_S_Items 
                 set stock = stock - (select IFNULL(quantity, 0) FROM IS601_S_Cart WHERE item_id = IS601_S_Items.id and user_id = %(uid)s) 
                 WHERE id in (SELECT item_id from IS601_S_Cart where user_id = %(uid)s)
-            """, {"uid":current_user.get_id()} )
+            """, {"uid": current_user.get_id()})
             if not result.status:
                 flash("Error updating stock", "danger")
                 has_error = True
@@ -228,7 +243,8 @@ def purchase():
         # apply purchase (specific to my project)
         if not has_error:
             # here I'm using a known item_id to update my player's stats
-            attrs = [("life", -1), ("speed", -2), ("fire_rate", -3), ("damage", -4), ("radius", -5)]
+            attrs = [("life", -1), ("speed", -2), ("fire_rate", -3),
+                     ("damage", -4), ("radius", -5)]
             for attr, target_id in attrs:
                 try:
                     query = f"""
@@ -241,18 +257,20 @@ def purchase():
                     """
                     print(f"{attr} query", query)
                     result = DB.insertOne(query,
-                    {"uid": current_user.get_id(),
-                    "attr": attr,
-                    "target_id": int(target_id)})
+                                          {"uid": current_user.get_id(),
+                                           "attr": attr,
+                                           "target_id": int(target_id)})
                 except Exception as e:
                     print(f"Error updating attribute {attr}", e)
         # empty the cart
         if not has_error:
-            result = DB.delete("DELETE FROM IS601_S_Cart WHERE user_id = %s", current_user.get_id())
-    
+            result = DB.delete(
+                "DELETE FROM IS601_S_Cart WHERE user_id = %s", current_user.get_id())
+
         if not has_error:
-            details = f"Spent {total} on {quantity} upgrades" # TBD
-            current_user.account.remove_points(-total, reason="purchase", details=details)
+            details = f"Spent {total} on {quantity} upgrades"  # TBD
+            current_user.account.remove_points(-total,
+                                               reason="purchase", details=details)
             DB.getDB().commit()
             flash("Purchase successful!", "success")
         else:
@@ -262,8 +280,10 @@ def purchase():
         flash("Something went wrong", "danger")
         traceback.print_exc()
     # TODO route to thank you / summary page
+
     # TODO add link from cart page to this route
     return render_template("order_summary.html", rows=cart, order=order)
+
 
 @shop.route("/orders", methods=["GET"])
 @login_required
@@ -279,6 +299,7 @@ def orders():
         print("Error getting orders", e)
         flash("Error fetching orders", "danger")
     return render_template("orders.html", rows=rows)
+
 
 @shop.route("/order", methods=["GET"])
 @login_required
