783c7565ac856c2288938171960447d23c50e829
KSHITIJ AJI <ka598@njit@edu>
2022-12-17 19:30:26 -0500

    Removed test_edit_page

--- IS601ShopProject/test/test_edit_page.py
+++ IS601ShopProject/test/test_edit_page.py
@@ -1,7 +1,6 @@
+
+'''
 import pytest
-
-
-
 @pytest.fixture()
 def app():
     from ..main import create_app
@@ -12,32 +11,29 @@ def app():
     })"""
     # insert dummy record to test at a negative index so it doesn't collide with valid values
     # note: this will likely still trigger auto_increment
-    DB.insertOne("INSERT INTO IS601_Sample (id, name, val) VALUES (-1, 'tc','tcval')")
+    DB.insertOne(
+        "INSERT INTO IS601_Sample (id, name, val) VALUES (-1, 'tc','tcval')")
     # other setup can go here
 
     yield app
 
     # clean up / reset resources here
     DB.delete("DELETE FROM IS601_Sample WHERE id = -1")
-
-
 @pytest.fixture()
 def client(app):
     return app.test_client()
-
-
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
-
 def test_edit_page(client):
     # https://www.scrapingbee.com/blog/python-web-scraping-beautiful-soup/
     response = client.get("/sample/edit?id=-1")
     from bs4 import BeautifulSoup
     soup = BeautifulSoup(response.data, 'html.parser')
-   
+
     form = soup.form
     ele = form.select("[name='value']")[0]
     print(ele)
     # for easier debugging run pytest with the -rP flags
     assert ele.get("value") == "tcval"
+'''
