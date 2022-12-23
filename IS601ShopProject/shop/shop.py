ac44ca6f1eb3031536938f30275012fad3b5d171
KSHITIJ AJI <ka598@njit@edu>
2022-12-22 21:29:17 -0500

    Made edits

--- IS601ShopProject/shop/shop.py
+++ IS601ShopProject/shop/shop.py
@@ -272,7 +272,7 @@ def purchase():
             current_user.account.remove_points(-total,
                                                reason="purchase", details=details)
             DB.getDB().commit()
-            #flash("Purchase successful!", "success")
+            flash("Purchase successful!", "success")
         else:
             return redirect(url_for("shop.cart"))
     except Exception as e:
